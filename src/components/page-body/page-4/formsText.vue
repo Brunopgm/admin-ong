d2f460f4e46ea50475cc713e2610db855617ec61
Brunopgm <bruno.pgm@outlook.com>
2020-10-30 16:22:42 -0300

    layout do formText criado

--- src/components/page-body/page-4/formsText.vue
+++ src/components/page-body/page-4/formsText.vue
@@ -1,23 +1,45 @@
 <template>
     <div class="page-content">
         <v-container fluid>
-            <h1 class="title">Formulário "Equipe de voluntários"</h1>
+            <div class="text-form">
+                <h3 class="title-form">Formulário "Equipe de voluntários"</h3>
+                <v-icon class="icon-open">
+                    mdi-format-quote-open-outline
+                </v-icon>            
+                <span>{{textWhereToFindUs}}</span>
+                <v-icon class="icon-close">
+                    mdi-format-quote-close-outline
+                </v-icon>
+            </div>
+
             <v-textarea
             class="mt-5"
             name="input-7-1"
             filled
             label="Equipe de voluntários"
-            value="Somos uma organização que funciona exclusivamente através de doações, inclusive de tempo e dedicação de uma equipe comprometida em ajudar o próximo e fazer a diferença na sociedade. Nossa estrutura conta com Presidente, Vice-Presidente, Secretários, Tesoureiros, Conselho Fiscal e mais de 80 voluntários. Seja um voluntário você também."
+            
+            v-model="textWhereToFindUs"
             ></v-textarea>
         </v-container>
 
         <v-container fluid>
-            <h1 class="title">Formulário "Onde nos encontrar"</h1>
+            <div class="text-form">
+                <h3 class="title-form">Formulário "Onde nos encontrar"</h3>
+
+                <v-icon class="icon-open">
+                    mdi-format-quote-open-outline
+                </v-icon>
+                <span class="text-form">{{textVoluntersTeam}}</span>
+                <v-icon class="icon-close">
+                    mdi-format-quote-close-outline
+                </v-icon>
+            </div>
             <v-textarea
+            class="mt-5"
             name="input-7-1"
             filled
             label="Equipe de voluntários"
-            value="Caso tenha dúvidas, sugestões ou informações entre em contato conosco pelo WhastsApp ou preencha o formulário abaixo e envie-nos uma mensagem"
+            v-model=textVoluntersTeam
             ></v-textarea>
         </v-container>
     </div>
@@ -25,10 +47,28 @@
 
 <script>
 export default {
-
+    data:()=>({
+        textVoluntersTeam: "Caso tenha dúvidas, sugestões ou informações entre em contato conosco pelo WhastsApp ou preencha o formulário abaixo e envie-nos uma mensagem",
+        textWhereToFindUs:"Somos uma organização que funciona exclusivamente através de doações, inclusive de tempo e dedicação de uma equipe comprometida em ajudar o próximo e fazer a diferença na sociedade. Nossa estrutura conta com Presidente, Vice-Presidente, Secretários, Tesoureiros, Conselho Fiscal e mais de 80 voluntários. Seja um voluntário você também."
+    })
 }
 </script>
 
 <style>
+    .icon-open{
+        top: -8px;
+    }
+    .icon-close{
+        bottom: -5px
+    }
+    .text-form{
+        min-height: 50px;
+        /* padding-bottom: 20px; */
+    }
 
+    @media(min-width: 600px){
+        .title-form{
+            text-align: right;
+        }
+    }
 </style>
