d4feed3a8d2d7cc4ed7ebf1cc022a81ae9f9fc6c
Brunopgm <bruno.pgm@outlook.com>
2020-12-03 22:17:37 -0300

    Layout do modal feito

--- src/components/page-body/projects/ProjectsCards.vue
+++ src/components/page-body/projects/ProjectsCards.vue
@@ -19,7 +19,7 @@
                             icon
                             color="pink"
                         >
-                            <v-icon>mdi-heart</v-icon>
+                            <v-icon>mdi-star</v-icon>
                         </v-btn>
                     </div>
                     <h3 class="text-justify">{{ title }}</h3>
@@ -29,34 +29,115 @@
          
             <v-divider class="my-4"></v-divider>
             
-            <div class="container-buttons">
-                <div class="buttons-edit">
-                    <v-btn 
-                        text
-                    >
-                        <v-icon class="mr-1">mdi-pencil</v-icon>
-                        Editar
-                    </v-btn>
-                    <v-btn 
-                        text
-                    >
-                        <v-icon class="mr-1">mdi-trash-can</v-icon>
-                        Excluir
-                    </v-btn>
-                </div>
+            
+            
+            <div>
 
-                <div
-                    class="button-featured desktop-featured"
-                >
-                    <v-btn
-                        icon
-                        color="pink"
+                <v-row justify="center">
+                    <v-dialog
+                        v-model="dialog"
+                        persistent
+                        max-width="800px"
                     >
-                        <v-icon>mdi-star</v-icon>
-                    </v-btn>
-                </div>
+                        <template v-slot:activator="{ on, attrs }" class="container-buttons-card">
+            
+                        <div class="container-buttons">
+                            <div class="buttons-edit">
+                                <v-btn
+                                    v-bind="attrs"
+                                    v-on="on" 
+                                    text
+                                >
+                                    <v-icon class="mr-1">mdi-pencil</v-icon>
+                                    Editar
+                                </v-btn>
+                                <v-btn 
+                                    text
+                                >
+                                    <v-icon class="mr-1">mdi-trash-can</v-icon>
+                                    Excluir
+                                </v-btn>
+                            </div>
+
+                            <div
+                                class="button-featured desktop-featured"
+                            >
+                                <v-btn
+                                    icon
+                                    color="pink"
+                                >
+                                    <v-icon>mdi-star</v-icon>
+                                </v-btn>
+                            </div>
+                        </div>
+                    
+                    
+                        </template>
+                    
+                        <v-card>
+                            <v-card-title>
+                                <span>Modifique o projeto</span>
+                            </v-card-title>
+                        
+                            <v-divider class="mb-4"></v-divider>
+
+                            <div class="container-image-modal">
+                                <v-img
+                                    class="mb-4 rounded"
+                                    max-height="500px"
+                                    max-width="100%"
+                                    :src="image"
+                                ></v-img>
+                            </div>
+
+                            <v-card-text>
+                                <v-container>
+                                    <p>Título</p>
+                                    <v-text-field
+                                        outlined
+                                        dense
+                                        required
+                                        v-model="title"
+                                    ></v-text-field>
+
+                                    <p>Digite aqui o texto do projeto</p>
+                                    <v-textarea
+                                        auto-grow
+                                        outlined
+                                        rows="5"
+                                        row-height="30"
+                                        required
+                                        v-model="text"
+                                    ></v-textarea>
+                            
+                                </v-container>
+                            </v-card-text>
+                        
+                            <v-divider class="mb-4"></v-divider>
+                            <v-card-actions class="container-buttons-modal">
+                                <v-spacer></v-spacer>
+
+                                <v-btn
+                                color="blue darken-1"
+                                text
+                                @click="[dialog = false]"
+                                >
+                                Fechar
+                                </v-btn>
+                                <v-btn
+                                color="blue darken-1"
+                                text
+                                >
+                                Salvar
+                                </v-btn>
+                            </v-card-actions>
+                        </v-card>
+                    </v-dialog>
+                </v-row>
             </div>
+        
         </v-card>
+        
     </div>
 </template>
 
@@ -67,7 +148,10 @@ export default {
         title: {type: String},
         text: {type: String},
         featured: {type: Boolean}
-    }
+    },
+    data: () => ({
+        dialog: false
+    })
 }
 </script>
 
@@ -87,12 +171,22 @@ export default {
         overflow: hidden;
         text-overflow: ellipsis;
     }
+    .container-buttons{
+            width: 100%;
+    }
+
     .button-featured{
             text-align: end;
         }
     .desktop-featured{
         display: none;
     }
+    .container-image-modal{
+        width: 95%;
+        margin: 0 auto;
+    }
+    
+
     @media(min-width: 370px){
         .container-buttons{
             display: block;
