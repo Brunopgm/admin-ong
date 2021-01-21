1ebbd7a0cb757f1edc5b68c299db02222667006a
Brunopgm <bruno.pgm@outlook.com>
2021-01-21 15:59:40 -0300

    Refatorando componente Home e Menu

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,29 +8,7 @@
 </template>
 
 <script>
-import FirebaseApp from "../firebaseConfig"
 export default {
-    created() {
-        console.log(FirebaseApp)
-        // const menu = [
-        //     {title: 'Home', url: '/home'},
-        //     {title: 'Sobre nós', url: '/sobre'},
-        //     {title: 'Projetos e ações', url: '/projetos-acoes'},
-        //     {title: 'Voluntários', url: '/voluntarios'},
-        //     {title: 'Sobre nós', url: '/contatos'}]
-        
-
-        //  const update = async () => {
-        //     await FirebaseApp.db.collection("users").doc('2NZePxJ7KU9UF9LD8UPt').set({name: 'BB'}).then(response => {
-        //         console.log('update', response)
-        //     })
-        // }
-
-        // create()
-        // read()
-        // update()
-        // remove()
-    }
 }
 
 </script>
